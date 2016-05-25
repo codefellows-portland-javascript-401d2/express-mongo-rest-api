928640d504c12c567a9294d835112ccfa2941825
DonChatelain <djchatelain@gmail.com>
2016-05-25 14:34:39 -0700

    adds monster route with post handled

--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
+const users = require('./routes/users');
+const monsters = require('./routes/monsters');
+
+app.use('/users', users);
+
+app.use('/monsters', monsters);
 
 module.exports = app;
---
+++ server/routes/monsters.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const bodyParser = require('body-parser').json();
+const Monster = require('../models/monster-schema');
+
+router
+    .post('/', bodyParser, (req, res) => {
+      new Monster(req.body).save()
+          .then(data => {
+            res.json({posted: data});
+          }).catch(err => {
+            console.log({error: err});
+          });
+    });
+    
+    
+module.exports = router;
