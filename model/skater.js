f6970b34a587a340ed13c37af49bba324418e89f
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-05-25 15:38:23 -0700

    Completed Skater model

---
+++ model/skater.js
@@ -0,0 +1,33 @@
+const mongoose = ('mongoose');
+const Schema = mongoose.Schema;
+
+var Skater = mongoose.model('Skater', new Schema( {
+
+  name: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: Number,
+    required: true
+  },
+  team: {
+    type: Schema.Types.ObjectId,
+    ref: 'Team'
+  },
+  positions: {
+    type: [String]
+  },
+  captain: {
+    type: Boolean
+  },
+  retired: {
+    type: Boolean
+  }
+},
+  {
+    timestamps: true
+  }
+));
+
+module.exports = Skater;
