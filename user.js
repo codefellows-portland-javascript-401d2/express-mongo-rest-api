9d2d3bcb7ea162fb34336159a26991e7d54f9fc0
DonChatelain <djchatelain@gmail.com>
2016-05-25 13:48:52 -0700

    fixes indent and lint issues

--- user.js
+++ user.js
@@ -2,9 +2,9 @@ const gen = require('./generator');
 
 class User {
     constructor(seed) {
-        this.name = gen.nameGen(seed); 
-        this.rank = gen.rankGen(seed);
-        this.dateJoined = gen.dateGen(seed, new Date(2010, 1, 1));
+      this.name = gen.nameGen(seed); 
+      this.rank = gen.rankGen(seed);
+      this.dateJoined = gen.dateGen(seed, new Date(2010, 1, 1));
     }
 
 }
