3d152af246c185e08843efbca7d3b1daec814b5a
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-05-25 16:49:05 -0700

    Changed mongodb port and Team.js color field

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('./lib/setup-mongoose');
 const app = require('./lib/app');
 const port = 8080;    //any merit to setting env right now?
 
-mongoose('mongodb://localhost:8080');
+mongoose('mongodb://localhost:27017');
 
 app.listen(port, () =>{
   console.log(`server be servin on port ${port}`);
