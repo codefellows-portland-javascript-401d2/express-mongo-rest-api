5beca4e4f153cfe0bb3b0d238f157ff95bb7f0f1
Allison Davis <a.rae.davis@gmail.com>
2016-05-25 15:43:38 -0700

    add raw data files

---
+++ rawData/teams.js
@@ -0,0 +1,28 @@
+db.teams.insert({
+  teamName: "High Rollers" ,
+  color: [{base: "green" }, {accent: "gold"}],
+  rank: 1,
+  currentWins: 4,
+  currentLosses: 2,
+});
+db.teams.insert({
+  teamName: "Break Neck Betties",
+  color: [{base: "red" }, {accent: "b&w checker"}],
+  rank: 4,
+  currentWins: 2,
+  currentLosses: 4,
+});
+db.teams.insert({
+  teamName: "Guns n Rollers",
+  color: [{base: "black" }, {accent: "pink" }],
+  rank: 2,
+  currentWins: 4,
+  currentLosses: 2,
+});
+db.teams.insert({
+  teamName: "Heartless Heathers",
+  color: [{base: "blue"}, {accent: "silver"}],
+  rank: 3,
+  currentWins: 2,
+  currentLosses: 4,
+});
