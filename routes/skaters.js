ad078c0fe870248a8f894b42e1aaa56e639deffe
Allison Davis <a.rae.davis@gmail.com>
2016-05-26 14:31:35 -0700

    PUT /skaters/:id complete

--- routes/skaters.js
+++ routes/skaters.js
@@ -21,6 +21,13 @@ router
       });
   })
 
+  .put('/skaters/:id', bodyParser, (req, res) => {
+    Skater.findOneAndUpdate(byId(req), req.body, [{new: true}, {upsert: true}])
+      .then((skaters) => {
+        res.json(skaters);
+      });
+  })
+
   .post('/skaters', bodyParser, (req, res) =>{
     new Skater(req.body).save()
       .then((skaters) => {
@@ -33,18 +40,9 @@ router
   });
 
 
-
-//.put
-
-
-//.delete
-
-
 //.patch
 
 module.exports = router;
-// GET: /skaters/  ->  returns JSON list of all skater names (and info?)
-// GET: /skaters/<id>  ->  returns JSON object of id
 // POST: /skaters/ (JSON body)  ->  returns JSON object (with db generated fields like id)
 // DELETE: /skaters/<id>  ->  returns JSON object ... or .... JSON message with name and id of deleted object
 // PUT: /skaters/<id> (JSON body)  ->  returns JSON object
