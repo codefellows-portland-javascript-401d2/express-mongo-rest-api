59204e55d01153fded5ab6a814f26afd3a687e1d
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-05-31 14:04:07 -0700

    Progress on auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,53 @@
 const router = require('express').Router();
 const bodyparser = require('body-parser');
 const User = require('../model/user');
+const mongoose = require('../lib/setup-mongoose');
 
+// mongoose(process.env.M_LAB_MONGO_URI || 'mongodb://localhost/rose-city-rollers');
 
 
 router
-.post('/signup', bodyparser, (req, res) => {
-
-  // save pass, delete from
+.post('/signup', bodyparser, (req, res, next) => {
 
   // req body has user and pass
-  // create new user, with user
+
+  // save pass, delete from req.body
+  const user = req.body.user;
+  const password = req.body.password;
+  delete req.body.password;
+
+  mongoose.findOne( {user: user})
+    .then( existing => {
+      if (existing){
+        next('Error: that user name is already registered');
+      }else{
+
+        // create new user, with req.body.user
+        var user = new User( req.body );
+
+        // create the salted version of the password with the user.instance method
+        const hash = user.generateHash(pasword);
+
+        // save the User with salted password
+        return user.save()
+        .then( () => {
+
+        })
+        .catch( () => {
+          next('Database failed to save user credentials');
+        })
+      }
+    })
+
+
+
+  });
+
+
+
+  // error handling if user already exists
+
+
 
 
 })
