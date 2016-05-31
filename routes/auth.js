274154a6fda5c053d6d7d1c7788b801a5279ab68
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-05-31 14:48:47 -0700

    /signin path is working in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -31,9 +31,36 @@ router
   .catch( () =>  next('Database failed to save user credentials'));
 })
 
-.post('/signin', (req, res) => {
+.post('/signin', (req, res, next) => {
 
+  if (!req.body.password){
+    next('Please provide a password');
+    return;
+  }
 
+  const username = req.body.username;
+  const password = req.body.password;
+  delete req.body.password;
+
+  User.findOne( {username: username})
+  .then( user => {
+
+    if (!user){
+      next ('Failed to find that username and password');
+      return;
+    }
+
+    if (!user.compareHash(password)){
+      next ('Failed to find that username and password');
+      return;
+    }
+
+    token.sign(user)
+    .then( token => res.json({token}));
+  })
+  .catch( () => {
+    next('Error 500: Database failed to validate user');
+  });
 });
 
 module.exports = router;
