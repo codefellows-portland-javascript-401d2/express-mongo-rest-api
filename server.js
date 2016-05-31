093288535c9f48cd2b60ea3c89723fd311b2dde9
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-05-31 15:09:49 -0700

    ensureAuth.js middleware function is working for resource routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('./lib/setup-mongoose');
 const app = require('./lib/app');
 const port = 8080;    //any merit to setting env right now?
 
-mongoose('mongodb://localhost/rose-city-rollers');
+mongoose(process.env.M_LAB_MONGO_URI || 'mongodb://localhost/rose-city-rollers');
 
 app.listen(port, () =>{
   console.log(`server be servin on port ${port}`);
