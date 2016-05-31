1f97ac5cd895431f10985ec4f3e67fe3f00004d4
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-05-31 13:46:58 -0700

    Start on auth.js route

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const bodyparser = require('body-parser');
+const User = require('../model/user');
+
+
+
+router
+.post('/signup', bodyparser, (req, res) => {
+
+  // save pass, delete from 
+
+  // req body has user and pass
+  // create new user, with user
+
+
+})
+
+.post('/signin', bodyparser, (req, res) => {
+
+
+});
+
+
+
+
+module.exports = router;
